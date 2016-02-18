cf671f461c9e5c906565b3e3cccee4ce679b37a0
Shaan Appel <shaan.appel@yahoo.com>
2016-02-17 23:31:29 -0800

    Initial commit

---
+++ myFlash/myFlash/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  myFlash
+//
+//  Created by Shaan Appel on 2/14/16.
+//  Copyright © 2016 Shaan Appel. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func buttonPressed(sender: AnyObject) {
+        let device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        if (device.hasTorch) {
+            do {
+                try device.lockForConfiguration()
+                if (device.torchMode == AVCaptureTorchMode.On) {
+                    device.torchMode = AVCaptureTorchMode.Off
+                } else {
+                    try device.setTorchModeOnWithLevel(1.0)
+                }
+                device.unlockForConfiguration()
+            } catch {
+                print(error)
+            }
+        }
+    }
+
+}
+
